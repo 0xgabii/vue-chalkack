731bff516261e96443a80d7fe01021a59d7082ec
Go Eun-Seok <gomonk3037@gmail.com>
2017-07-13 20:21:03 +0900

    add modal for no name users

---
+++ src/components/UserPage/Modals/FirstTimeUser.vue
@@ -0,0 +1,35 @@
+<template>
+  <modal 
+    v-if="!me.displayName"
+    name="당신의 이름은 무엇인가요?"
+    @submit="updateName">
+    <input v-model="displayName" />
+  </modal>  
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  name: 'FirstTimeUserModal',
+  data: () => ({
+    displayName: '',
+  }),
+  computed: {
+    ...mapState('auth', [
+      'me',
+    ]),
+  },
+  methods: {
+    ...mapActions('auth', [
+      'updateAuthProfile',
+    ]),
+    updateName() {
+      this.updateAuthProfile({
+        ...this.me,
+        displayName: this.displayName,
+      });
+    },
+  },
+};
+</script>
---
+++ src/components/UserPage/Pages/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mainPage">
+
+    <first-time-user-modal />
+  </div>
+</template>
+
+<script>
+import FirstTimeUser from '../Modals/FirstTimeUser';
+
+export default {
+  name: 'MainPage',
+  components: {
+    FirstTimeUserModal: FirstTimeUser,
+  },
+};
+</script>
