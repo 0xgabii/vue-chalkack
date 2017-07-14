52eee7ca2b16e85c4f5a79e96309f47f1109984b
Go Eun-Seok <gomonk3037@gmail.com>
2017-07-14 20:20:20 +0900

    create modal component for create album

---
+++ src/components/UserPage/Modals/CreateAlbum.vue
@@ -0,0 +1,43 @@
+<template>
+  <modal 
+    class="createAlbum"
+    title="Create Your Album">
+
+    <div class="inputBox">
+      <label class="inputBox__guideText">Name</label>
+      <input
+        class="inputBox__typeText" 
+        v-model="name" />
+    </div>
+
+    <div class="inputBox">
+      <label class="inputBox__guideText">Tags</label>
+      <input
+        class="inputBox__typeText" 
+        v-model="tags" />
+    </div>
+
+    <template slot="footer">
+      <button class="cancel">Cancel</button>
+      <button class="submit" @click="createAlbum">Create</button>
+    </template>
+
+  </modal>  
+</template>
+
+<script>
+// import { mapActions } from 'vuex';
+
+export default {
+  name: 'CreateAlbumModal',
+  data: () => ({
+    name: '',
+    tags: '',
+  }),
+  methods: {
+    createAlbum() {
+
+    },
+  },
+};
+</script>
