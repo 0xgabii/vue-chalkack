7fda512b5cfc6ce7eac530bc03d0950c2286d987
Go Eun-Seok <gomonk3037@gmail.com>
2017-07-14 20:20:35 +0900

    move modal components to index page

--- src/components/UserPage/Pages/Main.vue
+++ src/components/UserPage/Pages/Main.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="mainPage">
 
-    <first-time-user-modal />
   </div>
 </template>
 
 <script>
-import FirstTimeUser from '../Modals/FirstTimeUser';
-
 export default {
   name: 'MainPage',
-  components: {
-    FirstTimeUserModal: FirstTimeUser,
-  },
 };
 </script>
--- src/components/UserPage/index.vue
+++ src/components/UserPage/index.vue
@@ -6,6 +6,10 @@
     <button @click="signOut">logout</button>
 
     <router-view></router-view>
+  
+    <!-- modal area -->
+    <first-time-user-modal />
+    <create-album-modal />
 
   </section>
 </template>
@@ -13,16 +17,24 @@
 <script>
 import { mapState, mapActions } from 'vuex';
 
+import FirstTimeUser from './Modals/FirstTimeUser';
+import CreateAlbum from './Modals/CreateAlbum';
+
 export default {
   name: 'UserPage',
   computed: {
     ...mapState('auth', [
-      'isAuthenticated',
       'me',
     ]),
   },
+  methods: {
+    ...mapActions('auth', [
+      'signOut',
+    ]),
+  },
   components: {
-    LayoutHeader,
+    FirstTimeUserModal: FirstTimeUser,
+    CreateAlbumModal: CreateAlbum,
   },
 };
 </script>
