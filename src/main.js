99bce1853d8763f87c48114d2508b2c90a97126b
Go Eun-Seok <gomonk3037@gmail.com>
2017-07-16 23:41:21 +0900

    user vuefire

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 
+import VueFire from 'vuefire';
+
+Vue.use(VueFire);
+
 import App from './App';
 
 import store from './store';
