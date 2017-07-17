3be8db94a54b830a171620fbb59a21d61dfcb08a
Go Eun-Seok <gomonk3037@gmail.com>
2017-07-18 02:08:40 +0900

    create text-input component

---
+++ src/components/Common/Inputs/Text.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="inputBox">
+    <label class="inputBox__guideText">{{label}}</label>
+    <input
+      class="inputBox__typeArea" 
+      :value="value"
+      :readonly="readOnly"
+      @input="updateValue($event.target.value)" /> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextInput',
+  props: {
+    label: {
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+    readOnly: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  methods: {
+    updateValue(value) {
+      this.$emit('input', value);
+    },
+  },
+};
+</script>
