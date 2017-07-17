0be38a690d2ea713f3678c14fcb39aeaea44a67d
Go Eun-Seok <gomonk3037@gmail.com>
2017-07-17 20:39:12 +0900

    add tags input

--- src/components/UserPage/Modals/CreateAlbum.vue
+++ src/components/UserPage/Modals/CreateAlbum.vue
@@ -12,9 +12,18 @@
 
     <div class="inputBox">
       <label class="inputBox__guideText">Tags</label>
-      <input
-        class="inputBox__typeText" 
-        v-model="tags" />
+      <div class="tagsInput">
+        <div class="tagsInput-list">
+          <span v-for="(item, index) in tags.list" @click="removeTag(index)">
+            {{item}}
+          </span>
+        </div>
+        <input
+          class="tagsInput__typeTag" 
+          v-model="tags.input" 
+          @keydown.enter="addTag" 
+          @keydown.tab.prevent="addTag" />
+      </div>
     </div>
 
     <template slot="footer">
@@ -33,7 +42,11 @@ export default {
   name: 'CreateAlbumModal',
   data: () => ({
     name: '',
-    tags: '',
+    tags: {
+      input: '',
+      list: [],
+      max: 4,
+    },
   }),
   computed: {
     ...mapState('auth', [
@@ -41,12 +54,22 @@ export default {
     ]),
   },
   methods: {
+    addTag() {
+      const { input, list, max } = this.tags;
+      if (list.length < max) {
+        this.tags.list.push(input);
+        this.tags.input = '';
+      }
+    },
+    removeTag(index) {
+      this.tags.list.splice(index, 1);
+    },
     createAlbum() {
       const { name, tags, me } = this;
 
       albums.create({
         name,
-        tags: tags.split(','),
+        tags: tags.list,
         user: me.id,
       });
     },
