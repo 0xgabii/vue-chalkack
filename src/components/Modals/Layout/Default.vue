0d18efb9461243edcfb5b00a2a0774bba618a8dd
Go Eun-Seok <gomonk3037@gmail.com>
2017-07-19 21:33:30 +0900

    add backdrop for catch click

--- src/components/Modals/Layout/Default.vue
+++ src/components/Modals/Layout/Default.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="modal">
-    <div class="modal-wrapper">
-      <header class="modal-header">
-        <h3 class="modal-header__title">{{ title }}</h3>
-        <p class="modal-header__subtitle">{{ subtitle }}</p>
-      </header>
-      <section class="modal-content">                
-        <slot />
-      </section>
-      <footer class="modal-footer">
-        <slot name="footer">
-          <button class="close" @click="handleClose">Submit</button>
-          <button class="submit" @click="handleSubmit">Submit</button>
-        </slot>
-      </footer>
+  <transition name="modal">
+    <div class="modal">
+      <div class="modal__backdrop" @click="handleClose" />
+      <div class="modal-wrapper">
+        <header class="modal-header">
+          <h3 class="modal-header__title">{{ title }}</h3>
+          <p class="modal-header__subtitle">{{ subtitle }}</p>
+        </header>
+        <section class="modal-content">                
+          <slot />
+        </section>
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button class="close" @click="handleClose">Submit</button>
+            <button class="submit" @click="handleSubmit">Submit</button>
+          </slot>
+        </footer>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
