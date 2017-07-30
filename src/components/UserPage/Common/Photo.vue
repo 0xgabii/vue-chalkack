e34bf07aa85dab47d69f684c24bd9d8b64796685
Go Eun-Seok <gomonk3037@gmail.com>
2017-07-31 00:47:32 +0900

    Create Photo component

---
+++ src/components/UserPage/Common/Photo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="photo">
+    
+    <img :src="data.src" />
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Photo',
+  props: {
+    data: {
+      src: String,
+      date: String,
+      color: Array,
+    },
+  },
+};
+</script>
