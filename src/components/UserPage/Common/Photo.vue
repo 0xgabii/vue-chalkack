ea4c05778b9ab2f6d7062e0f2ad2a49c70c70623
Go Eun-Seok <gomonk3037@gmail.com>
2017-08-01 23:55:14 +0900

    Add checkbox for select photo component

--- src/components/UserPage/Common/Photo.vue
+++ src/components/UserPage/Common/Photo.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="photo" :style="photoStyle">
-    
+  <div 
+    class="photo" 
+    :class="{ selected: checked }"
+    :style="photoStyle">
+
+    <div class="checkbox">
+      <label class="checkbox__control">
+        <input type="checkbox" v-model="checked" />
+      </label>
+    </div>
+
     <img :src="data.src" />
+
   </div>
 </template>
 
@@ -17,6 +27,9 @@ export default {
     },
     height: Number,
   },
+  data: () => ({
+    checked: false,
+  }),
   computed: {
     photoStyle() {
       const { height, data } = this;
